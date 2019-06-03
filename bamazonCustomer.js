8765e2305a35b6e1a52796ce2abaabe3c1583b8e
kellidragon <dragonkelli@gmail.com>
2019-06-02 21:00:47 -0700

    need to update mysql + check quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,10 +12,10 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("Welcome to Bamazon, the world's larget online store!")
+    console.log("--------------------------------------------------------\n")
+    console.log("\nWELCOME TO BAMAZON, THE WORLD'S LARGEST ONLINE STORE!\n")
+    console.log("--------------------------------------------------------\n")
     showInventory();
-    placeOrder();
-    // console.log("connected as id " + connection.threadId + "\n");
 });
 
 //function to see if the user would like to check stock
@@ -32,11 +32,14 @@ function showInventory() {
         ])
         .then(function (inquirerResponse) {
             if (inquirerResponse.confirm) {
-                console.log("All Products: \n");
-                console.log("ID#   ||   Name   ||    Price \n");
+                console.log("\n \n PRODUCTS LIST: ");
+                console.log("---------------------------\n")
+                console.log("ID#   ||   Name   ||    Price");
+                console.log("--------------------------------\n")
                 connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
                     if (err) throw err;
                     console.log(res);
+                    placeOrder();
                     connection.end();
                 });
             }
@@ -48,43 +51,128 @@ function showInventory() {
 }
 
 function placeOrder() {
-    console.log("PLACE AN ORDER: \n")
-    inquirer
-        .prompt([
-            {
-                type: "checkbox",
-                message: "What is the ID# of the product(s) you would like to buy?",
-                choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-                name: "selected items"
-            },
-        ])
-        .then(function (inquirerResponse) {
-            if (inquirerResponse.confirm) {
-                addQuantity();
-            }else {
-            console.log("\nThat's okay, maybe next time.\n");
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    type: "confirm",
+                    message: "\n \n READY TO PLACE AN ORDER?\n \n",
+                    name: "confirm",
+                    default: true
+                },
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    message: "\nSelect the ID# of the product(s) you would like to purchase: ",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].item_id);
+                        }
+                        return choiceArray;
+                    }
+                },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many units would you like?",
+                    // validate: function (value) {
+                    //     connection.query("SELECT stock_quantity FROM products", function (err, res) {
+                    //         if (err) throw err;
+                    //         console.log(res)
+                    //         // if (parseInt(value) > res) {
+                            //     console.log("Sorry, insufficient quantity, check back later.")
+                            // }
+                            // } else {
+                            //     console.log(res);
+                            //     // connection.end();
+                            // }
+                        // })
+                    // }
+                }
+
+
+            ]).then(function (answer) {
+                // get the information of the chosen item
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_id === answer.choice) {
+                        chosenItem = results[i];
+                        console.log("\n\n YOU HAVE SELECTED THE FOLLOWING: \n")
+                        console.log("--------------------------------")
+                        console.log(answer.quantity + chosenItem);
+                        console.log("YOUR TOTAL IS: ")
+                    }
+
+                }
+                inquirer
+                    .prompt([
+                        {
+                            type: "confirm",
+                            message: "PLACE ORDER?",
+                            name: "confirm",
+                            default: true
+                        }
+                    ])
             }
-         });
-
+            )
+    }
+    )
 }
+//CHECK THE QUANITITY IS AVAIL (fix validate section)
+//TODO: PRETTY UP INVENTORY LIST
+//TODO: UPDATE DATABASE WITH QUANTITY INFO
+//SHOW TOTAL PRICE OF ITEMS
+
+                    // if (chosenItem.highest_bid < parseInt(answer.bid)) {
+                    //     // bid was high enough, so update db, let the user know, and start over
+                    //     connection.query(
+                    //       "UPDATE auctions SET ? WHERE ?",
+                    //       [
+                    //         {
+                    //           highest_bid: answer.bid
+                    //         },
+                    //         {
+                    //           id: chosenItem.id
+                    //         }
+                    //       ],
+
+                    // .then(function (inquirerResponse) {
+                // if (inquirerResponse.confirm) {
+                //     connection.query("SELECT * from products where ?"
+                //     [
+                //         {
+                //             item_id: "selectedProducts"
+                //         }
+                //     ],
+                //         function (err, res) {
+                //             // if (err) throw err;
+                //             console.log("\n\n YOU HAVE SELECTED THE FOLLOWING ITEM(S): \n" + res);
+                //             // addQuantity();
+                //         });
+                // } else {
+                //     console.log("\nThat's okay, maybe next time.\n");
+                // }
 
 
-function addQuantity() {
-    
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "How many units would you like?",
-            name: "quantity"
-        },
-    ]).then(function (inquirerResponse) {
-        if (inquirerResponse.confirm) {
-            console.log("All Products: \n");
-            console.log("ID#   ||   Name   ||    Price \n");
-            connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
-                if (err) throw err;
-                console.log(res);
-                connection.end();
-            });
-        }
+
+// function addQuantity() {
+
+//     inquirer
+//         .prompt([
+//             {
+//                 type: "input",
+//                 message: "How many units would you like?",
+//                 name: "quantity"
+//             },
+//         ]).then(function (inquirerResponse) {
+//             if (inquirerResponse.confirm) {
+//                 console.log("You've selected \n");
+//                 console.log("ID#   ||   Name   ||    Price \n");
+//                 connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
+//                     if (err) throw err;
+//                     console.log(res);
+//                     connection.end();
+//                 });
+//             
